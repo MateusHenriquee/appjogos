c2faa911ec6d79bfd10416bbaaaa991e3701214b
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2025-09-30 09:52:36 -0300

    javaa

---
+++ jogos/src/testes/janelateste.java
@@ -0,0 +1,11 @@
+package testes;
+
+import javax.swing.JFrame;
+
+public class janelateste {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("neymar slk");
+        janela.setBounds(0,0,1920,180);
+        janela.setVisible(true);
+    }
+}
