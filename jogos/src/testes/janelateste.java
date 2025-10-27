c4ba01cbff42777b52b03f88299d06e9d41f0525
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2025-10-27 07:41:17 -0300

    Update janelateste.java

--- jogos/src/testes/janelateste.java
+++ jogos/src/testes/janelateste.java
@@ -4,10 +4,9 @@ import javax.swing.JFrame;
 
 public class janelateste {
     public static void main(String[] args) {
-        //JFrame janela = new JFrame("neymar slk");
-        //janela.setBounds(0,0,1920,1080);
-        //janela.setVisible(true);
-        Janela janela = new Janela();
+        JFrame janela = new JFrame("neymar slk");
+        janela.setBounds(0,0,1920,1080);
+        janela.setVisible(true);
         
     }
 }
