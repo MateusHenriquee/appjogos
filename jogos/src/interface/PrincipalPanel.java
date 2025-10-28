03abadeb80325c7009596bccacc9f6590328b354
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2025-10-28 09:55:07 -0300

    cu

--- jogos/src/interface/PrincipalPanel.java
+++ jogos/src/interface/PrincipalPanel.java
@@ -1,4 +1,6 @@
 
+package inteface;
+
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -6,15 +8,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PrincipalPanel extends TelaPanel{
-    public PrincipalPanel(JPanel telas, JFrame janela){
-        super(telas,janela);
-        JLabel mensagem = new JLabel("Conteúdo de Principal Panel");
-        JButton botao = new JButton("voltar");
-        botao.addActionListener(this);
+
+    public PrincipalPanel(JPanel telas, JFrame janela){//jpanel telas para ser exibido na trca de telas
+        super(telas, janela);
+        JLabel mensagem = new JLabel(" Conteúdo do principal Panel");//CRIA A MENSAGEM
+        JButton botao = new JButton("voltar"); //CRIA UM BOTÃO
+        botao.addActionListener(this);//chama o método override
+
         this.add(botao);
-        this.add(mensagem);
+        this.add(mensagem);//ADICIONA A MENSAGEM NA TELA
     }
+
     public void executarBotao(ActionEvent e){
-        trocaTela("Tela Login");
+        trocarTela("Tela Login");
     }
 }
